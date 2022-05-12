56b9fd87972632ac7d6643ee977fae88ca07d55e
Blastose <blastoisejay@gmail.com>
2022-05-11 19:11:28 -0700

    Add ability to hide content items in small screens

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ function toggleSidebarItems() {
   sidebarItems.classList.toggle('hide-small');
 }
 
+function toggleContentItems(elementParent) {
+  elementParent.classList.toggle('hide');
+  const contentItem = document.querySelector(`.${elementParent.getAttribute("data-content-name")}`);
+  contentItem.classList.toggle('hide-small');
+}
+
 const sidebarButton = document.getElementById('sidebar-btn');
 sidebarButton.addEventListener('click', toggleSidebarItems);
+
+const contentSections = document.querySelectorAll('.content-title');
+contentSections.forEach((element) => {
+  element.addEventListener('click', e => {
+    toggleContentItems(e.currentTarget);
+  });
+});
